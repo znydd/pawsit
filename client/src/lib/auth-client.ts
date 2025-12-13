25b67fa0b8f358b8aea63b43493f3fa3acf238b5
Znyd <mailboxofnabil@gmail.com>
2025-12-14 02:52:10 +0600

    fix: Vite env retrieve

--- client/src/lib/auth-client.ts
+++ client/src/lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-    baseURL: process.env.SERVER_BASE_URL,
+    baseURL: import.meta.env.VITE_SERVER_BASE_URL,
 
 });
 
